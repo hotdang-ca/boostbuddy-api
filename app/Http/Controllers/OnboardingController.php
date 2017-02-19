b5faa5cc6685022a46dc22dc28a4f4aee06504d2
James Robert Perih <james@hotdang.ca>
2017-02-19 14:52:31 -0600

    Accept the params

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -21,6 +21,86 @@ class OnboardingController extends Controller
       $this->SERVICE_TYPES = ['Boost', 'Tire Change', 'Fuel Delivery', 'Lock-out', 'Tow'];
     }
 
+    public function receiveServiceRequest(Request $request) {
+      $firstname = $request->firstname;
+      $lastname = $request->lastname;
+      $email = $request->email;
+      $phone = $request->phone;
+      $carDescription = $request->car_description;
+      $serviceType = $request->service_type;
+
+      $originLat = $request->origin['lat'];
+      $originLng = $request->origin['lng'];
+      $originLabel = $request->origin['label'];
+      $originDescription = $request->origin['description'];
+
+      // if it's a tow
+      if (strcmp($serviceType, "tow") == 0) {
+        $destinationLat = $request->destination['lat'];
+        $destinationLng = $request->destination['lng'];
+        $destinationLabel = $request->destination['label'];
+        $destinationDescription = $request->destination['description'];
+        $destinationQuotedDistance = $request->destination['quoted_distance'];
+      }
+
+      $jsonResponse = array();
+      $jsonResponse['firstname'] = $firstname;
+      $jsonResponse['lastname'] = $lastname;
+      $jsonResponse['email'] = $email;
+      $jsonResponse['phone'] = $phone;
+      $jsonResponse['carDescription'] = $carDescription;
+      $jsonResponse['serviceType'] = $serviceType;
+
+      $jsonResponse['origin'] = array();
+      $jsonResponse['origin']['lat'] = $originLat;
+      $jsonResponse['origin']['lng'] = $originLng;
+      $jsonResponse['origin']['label'] = $originLabel;
+      $jsonResponse['origin']['description'] = $originDescription;
+
+      if (strcmp($serviceType, "tow") === 0) {
+        $jsonResponse['destination'] = array();
+        $jsonResponse['destination']['lat'] = $destinationLat;
+        $jsonResponse['destination']['lng'] = $destinationLng;
+        $jsonResponse['destination']['label'] = $destinationLabel;
+        $jsonResponse['destination']['description'] = $destinationDescription;
+        $jsonResponse['destination']['quotedDistance'] = $destinationQuotedDistance;
+      }
+
+      // calculate some magic prices
+      // Hey Gents I've spent the last 4-5 hours playing around with pricing.
+      // I will be doing up a more formalized copy - but for your situational
+      // awareness this is the draft:
+      // Within the service area Cost to Customer $65 (Boost/Tire Change/Fuel Delivery/Lockout),
+      //  provider gets $55, leaving us with $10.
+      //  Tows must originate from the service area- Cost to Customer $99 for 20km or less,
+      //  $2.50/km beyond,
+      //  provider gets $84 and $2.00/km beyond 20km, we get $15 and $0.50/km beyond 20km.
+      //
+      // Basis is providers earn 85% revenue on flat fees and 80% on variable rates.
+      // Providers choose the radius they are willing to service for at these standard
+      // price points.
+      $price = 0;
+
+      if (strcmp($serviceType, 'tow') === 0) {
+        $price = 99;
+        if (intval($destinationQuotedDistance) > 20) {
+          $difference = intval($destinationQuotedDistance) - 20;
+          $price = $price + ($difference * 2.50);
+        }
+      } else {
+        $price = 65;
+      }
+
+      $jsonResponse['estimate'] = $price;
+      // $jsonResponse['GST'] = $price * 0.05;
+      // $jsonResponse['total'] = $price + ( $price * 0.05 );
+
+      // Make us an ORDER!
+      
+
+      return response()->json($jsonResponse);
+    }
+
     public function receiveNameAndLocation(Request $request) {
       $jsonResponse = array();
 
