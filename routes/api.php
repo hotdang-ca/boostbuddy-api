6c15ef258d2014d3bde02187706272ed334ef760
James Robert Perih <james@hotdang.ca>
2017-03-01 17:20:25 -0600

    a better CORS

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 
 use Illuminate\Http\Response;
 
-Route::group(['prefix' => 'v0'], function () {
+Route::group(['middleware' => 'cors', 'prefix' => 'v0'], function () {
     Route::post('/service/request', 'OnboardingController@receiveServiceRequest')->middleware('cors');
     Route::post('/service/request/{order}/pay', 'OnboardingController@markServiceRequestPaid')->middleware('cors');
     Route::get('/service/request/{order}/validate', 'OnboardingController@validateRequest');
     Route::get('/service/request/{order}/status', 'OnboardingController@showServiceRequestStatus');
     Route::post('/service/request/{order}/review', 'OnboardingController@reviewServiceRequest')->middleware('cors');
 
-    Route::post('/providers/add', 'ServiceProvidersApiController@addProvider')->middleware('cors');
+    Route::post('/providers/add', 'ServiceProvidersApiController@addProvider')- >middleware('cors');
 });
