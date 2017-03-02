fc2f23f9e29e2436dedfe97f8d6d81881b517285
James Robert Perih <james@hotdang.ca>
2017-03-02 16:52:59 -0600

    fix hardcoded ETA

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -26,10 +26,6 @@ class OnboardingController extends Controller
     public function showServiceRequestStatus(Request $request, $order) {
       $pendingOrder = DB::table('servicerequests')->where('order_number', $order)->first();
       if (isset($pendingOrder)) {
-        // TODO: need some more statuses for service requests
-        // add some more things
-        $pendingOrder->eta = 30;
-
         return response()->json($pendingOrder);
       } else {
         return response()->json(array());
