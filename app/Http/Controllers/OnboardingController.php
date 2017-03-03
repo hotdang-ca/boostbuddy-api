e5f7a0a1001f541ee54c9b5f97ef556f8d821a9b
James Robert Perih <james@hotdang.ca>
2017-03-03 13:44:46 -0600

    fix

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -249,15 +249,15 @@ class OnboardingController extends Controller
         }
 
         $results = DB::select("SELECT * FROM servicerequests WHERE order_number = '$uuid'");
-
+        $order = $results[0];
         // send mail to serviceproviders
 
         $providers = DB::select("SELECT name, email FROM serviceproviders");
 
         foreach ($providers as $provider) {
-          Mail::send('providers.emails.newservicerequest', ['provider' => $provider, 'order' => $results[0] ], function ($m) use ($user) {
+          Mail::send('providers.emails.newservicerequest', ['provider' => $provider, 'order' => $order ], function ($m) use ($provider, $order) {
             $m->from('hello@boostbuddy.ca', 'Boostbuddy Service');
-            $m->to($user->email, $user->name)->subject('New Boostbuddy Service Request!');
+            $m->to($order->email, "$order->firstname $order->lastname")->subject('New Boostbuddy Service Request!');
           });
         }
 
