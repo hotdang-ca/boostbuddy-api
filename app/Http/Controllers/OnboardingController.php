149d0c593026578ca4a87e731494422b31ae906d
James Robert Perih <james@hotdang.ca>
2017-03-03 13:52:30 -0600

    Use provider email, not customer email

--- app/Http/Controllers/OnboardingController.php
+++ app/Http/Controllers/OnboardingController.php
@@ -257,7 +257,7 @@ class OnboardingController extends Controller
         foreach ($providers as $provider) {
           Mail::send('providers.emails.newservicerequest', ['provider' => $provider, 'order' => $order ], function ($m) use ($provider, $order) {
             $m->from('hello@boostbuddy.ca', 'Boostbuddy Service');
-            $m->to($order->email, "$order->firstname $order->lastname")->subject('New Boostbuddy Service Request!');
+            $m->to($provier->email, $provider->name)->subject('New Boostbuddy Service Request!');
           });
         }
 
